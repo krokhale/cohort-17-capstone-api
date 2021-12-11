f88e38374e47944858a04910b13fb4962622bf3b
krokhale <krishna.rokhale@gmail.com>
2021-12-11 10:36:16 -0800

    checking in

---
+++ routes/api.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+
+let models = require('../lib/models')
+
+// RESTful API routes
+// 1) Create a question
+// POST http://localhost:3000/api/questions
+// 2) Fetch all the questions
+// GET http://localhost:3000/api/questions
+
+// 3) Create an answer for a question
+// POST http://localhost:3000/api/questions/45/answers // Create an answer for question number 45
+// POST http://localhost:3000/api/questions/:questionId/answers // We will have access to :questionId using req.params.questionId
+// models.Answer.create({questionId: req.params.questionId, questionText: req.body.questionText})
+// 4) Get all the answers for a question
+// GET http://localhost:3000/api/questions/:questionId/answers
+
+
+
+
+
+// POST http://localhost:3000/api/questions
+router.post('/questions', async function(req, res, next) {
+    console.log('req.body are', req.body)
+    let question = await models.Question.create({questionText: req.body.questionText})
+    res.json(question)
+});
+
+// Get http://localhost:3000/api/questions
+router.get('/questions', async function(req, res, next) {
+    let questions = await models.Question.findAll({})
+    res.json(questions)
+});
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.send('this is the API home page!')
+});
+
+module.exports = router;
